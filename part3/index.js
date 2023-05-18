240f470cfb798f347b99a5e1dc1ff8b1831b2486
Axel Tan <axeltanxl@gmail.com>
2023-05-18 12:01:14 +0800

    Complete exercises 3.19, 3.20, 3.21

--- part3/index.js
+++ part3/index.js
@@ -53,14 +53,8 @@ app.delete('/api/persons/:id', (request,response) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-
-    if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'name or number missing'
-        })
-    }
 
     const person = new Person({
         name: body.name,
@@ -71,18 +65,14 @@ app.post('/api/persons', (request, response) => {
         .then(savedPerson => {
             response.json(savedPerson)
         })
+        .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
 
-    const body = request.body
+    const { name, number } = request.body
 
-    const person = {
-        name: body.name,
-        number: body.number
-    }
-
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    Person.findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
@@ -95,6 +85,10 @@ const errorHandler = (error, request, response, next) => {
     if (error.name === 'CastError') {
         return response.status(400).send({
             error: 'malformatted id'
+        })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ 
+            error: error.message
         })
     }
 
--- part3/models/person.js
+++ part3/models/person.js
@@ -14,9 +14,40 @@ mongoose.connect(url)
         console.log('error connecting to MongoDB:', error.message)
     })
 
+// const personSchema = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         minLength: [3, 'Name must be at least 3 characters long!'],
+//         required: true
+//     },
+//     number: {
+//         type: String,
+//         minLength: [8, 'Phone number must be at least 8 digits long!'],
+//         validate: [{
+//             validator: (v) => {
+//                 return /^\d{2,3}-\d+$/.test(v)
+//             }},
+//             'Invalid format for the phone number!'
+//         ]
+//     }
+// })
+
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: (v) => {
+                return /^\d{2,3}-\d+$/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        }
+    }
 })
 
 personSchema.set('toJSON', {
